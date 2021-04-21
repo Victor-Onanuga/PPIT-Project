92af3388cb6d1330b6630b268e6cac8b78a054b6
Victor-Onanuga <44228722+Victor-Onanuga@users.noreply.github.com>
2021-04-21 19:01:48 +0100

    Hosting a game

---
+++ PPIT Unity Game/Assets/__Scripts/HostGame.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : NetworkBehaviour
+{
+    [SerializeField] private uint roomSize = 6;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if(networkManager.matchMaker == null)// checks to see if a room exists already
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    // Creating Room
+    public void CreateRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+            // Create room
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, " ", " ", " ", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
